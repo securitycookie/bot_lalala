741a38b305ebe7bd78e654367af49a250f9ce223
securitycookie <53958124+securitycookie@users.noreply.github.com>
2020-03-11 18:49:32 +0300

    BOT

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import vk_api
 from vk_api.longpoll import VkEventType, VkLongPoll
 
 session = requests.Session()
-vk_session = vk_api.VkApi(token = '29ed229ab090ede80113f9772a88043205ac028cde2fb479dd6f03f1674af904b1568489b99ccacfd866d')
+vk_session = vk_api.VkApi(token = '*')
 longPoll = VkLongPoll (vk_session)
 vk = vk_session.get_api()
 
@@ -106,109 +106,3 @@ for event in longPoll.listen():
     user.level_zero() if rsp == msg_unknown else user.level_up()
 
     vk.messages.send(user_id=event.user_id, random_id=randint(0,2**32), message=rsp)
-
-"""
-for event in longPoll.listen():
- if event.type ==  VkEventType.MESSAGE_NEW and event.to_me:
-    idu = event.user_id
-    lvl = check(idu, data)
-    
-    print(event.text, '--->', event.text.lower(), sep='')
-    print(f'lvl: {lvl}, type(lvl): {type(lvl)}, idu: {idu}')
-
-    response = ' '
-    if lvl == 0:
-        print('in lvl0')
-        if event.text.lower() == 'привет' or event.text.lower() == 'хай' or event.text.lower() == 'hi' or event.text.lower() == 'hello' or event.text.lower() == 'приветики':
-            response = 'привет! здесь ты можешь создавать тесты! Xочешь создать тест?'
-            print(11)
-            level(idu , data)
-        else : 
-            response = '???'
-    elif lvl == 1:
-        print(55)
-        if event.text.lower() ==  'хочу создать тест' or event.text.lower() ==  'да' or event.text.lower() == 'тест':
-            response = 'напиши имя своего теста: "имя текста:"'
-            level(idu, data)
-        else : 
-            lvl = 0 
-            response  = 'привет'
-    elif lvl == 2:
-        if event.text.lower() [0:11] == 'имя теста :':
-            name = input()
-            open (name+".txt", "w")
-            response = 'напиши язык : "язык:'
-            level(idu)
-        else :
-            lvl = 0
-    elif lvl == 3:
-        if event.text.lower() [0:5] == 'язык: ':
-            open (name+".txt", "w" )
-            language = input()
-            response = 'напиши категорию'
-            level(idu)
-        else :
-            lvl = 0
-    elif lvl == 4 :
-        if event.text.lower() [0:11] == 'категория: ':
-            category = input()
-            open (name+".txt","w")
-            response = 'напиши свой первый вопрос : "вопрос 1 :"'
-            level(idu)
-        else:
-            lvl = 0
-    elif lvl == 5:
-        if event.text.lower() [0:10] == 'вопрос 1 :':
-            question1 = input()
-            open (name+".txt","w" )
-            response = 'напиши верный ответ на вопрос'
-            level(idu)
-        else:
-            lvl = 0
-    elif lvl == 6:
-        if event.text.lower() [0:9] == 'ответ 1 :':
-            answer1 = input()
-            open (name+".txt", "w")
-            response = 'напиши следующий вопрос'
-            level(idu)
-        else:
-            lvl = 0
-    elif lvl == 7 :
-        if event.text.lower() [0:10] == 'вопрос 2 :':
-            question2 = input()
-            open (name+".txt","w" )
-            response = 'напиши верный ответ на вопрос'
-            level(idu)
-        else :
-            lvl = 0
-    elif lvl == 8:
-        if event.text.lower() [0:9] == 'ответ 2 :':
-            answer2 = input()
-            open (name+".txt", "w")
-            response = 'напиши следующий вопрос'
-            level(idu)
-        else :
-            lvl = 0
-    elif lvl == 9 :
-        if event.text.lower() [0:10] == 'вопрос 3 :':
-            question2 = input()
-            open (name+".txt", "w")
-            response = 'напиши верный ответ на вопрос'
-            level(idu)
-        else :
-            lvl = 0
-    elif lvl == 10 :
-        if event.text.lower() [0:9] == 'ответ 3 :':
-            answer2 = input()
-            open (name+".txt","w" )
-        else:
-            lvl = 0
-        
-
-    print(data)
-    info = f'{event.user_id} от {event.text} '
-    logging (info)
-    print(response)       
-    vk.messages.send(user_id=event.user_id, random_id=randint(0,2**32), message=response)
-    #eval(input(">>> "))
-"""
